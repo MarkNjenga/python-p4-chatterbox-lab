509bc6dea8189b62b75731a2118dab97655886da
Mark Njenga <marknjenga25@gmail.com>
2024-10-10 11:23:14 +0300

    "Update client and server code: modify db.json, update API endpoints and routes in App.js, EditMessage.js, Message.js, and NewMessage.js, and add CRUD functionality to server's app.py and models.py"

--- server/app.py
+++ server/app.py
@@ -14,13 +14,35 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+       messages = Message.query.order_by(Message.created_at.asc()).all()
+       return ([{'id': message.id, 'body': message.body, 'created_at': message.created_at} for message in messages])
+    elif request.method == 'POST':
+        if 'body' not in request.get_json() or 'username' not in request.get_json():
+            return jsonify({'message': 'Body and username are required'}), 400
+        new_message = Message(body=request.get_json()['body'], username=request.get_json()['username'])
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify({'id': new_message.id, 'body': new_message.body, 'username': new_message.username, 'created_at': new_message.created_at}), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = db.session.get(Message, id)
+    if message is None:
+        return jsonify({'message': 'Message not found'}), 404
+    if request.method == 'GET':
+        return jsonify({'id': message.id, 'body': message.body, 'username': message.username, 'created_at': message.created_at})
+    elif request.method == 'PATCH':
+        if 'body' in request.json:
+            message.body = request.json['body']
+        db.session.commit()
+        return jsonify({'id': message.id, 'body': message.body, 'username': message.username, 'created_at': message.created_at})
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({'message': 'Message deleted successfully', 'id': id}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
